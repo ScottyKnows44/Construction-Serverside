0e62e767b85d5a35ad055164b06ba0ab3845968c
ScottyKnows44 <Scottcfalcon@gmail.com>
2019-02-23 19:08:43 -0800

    server side, entered data from api

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 const bodyParser = ('body-parser');
+const superagent = require('superagent');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -17,6 +18,29 @@ client.on('error', err=> console.error(err));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+if (client.query(`SELECT COUNT (*) FROM seattle;`) < 1) {
+  let url = 'https://data.seattle.gov/resource/2ey2-escs.json';
+  superagent.get(url)
+    .then(data => {
+      data.body.forEach(object => {
+        client.query(`INSERT INTO seattle (location, dates, projectname, url, description) 
+        VALUES ($1, $2, $3, $4, $5)`,
+        [object.location,
+          object.dates,
+          object.projectname,
+          object.url,
+          object.description]),
+        err => console.error(err);
+      });
+    });
+}
+
+app.get('/projects/seattle', (req, res) => {
+  client.query(`SELECT location, dates, projectname, url, description FROM seattle;`)
+    .then(results => res.send(results.rows))
+    .catch(console.err);
+});
 
 app.get('/', (req,res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
