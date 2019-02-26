fae02d816728218400b30148a20ca2671f03e9c4
ScottyKnows44 <Scottcfalcon@gmail.com>
2019-02-25 18:07:47 -0800

    fixed sever.js to load DB

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use-strict';
 
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
-const bodyParser = ('body-parser');
+// const bodyParser = ('body-parser');
 const superagent = require('superagent');
 
 const app = express();
@@ -16,30 +18,34 @@ client.connect();
 client.on('error', err=> console.error(err));
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
-if (client.query(`SELECT COUNT (*) FROM seattle;`) < 1) {
-  let url = 'https://data.seattle.gov/resource/2ey2-escs.json';
-  superagent.get(url)
-    .then(data => {
-      data.body.forEach(object => {
-        client.query(`INSERT INTO seattle (location, dates, projectname, url, description) 
-        VALUES ($1, $2, $3, $4, $5)`,
-        [object.location,
-          object.dates,
-          object.projectname,
-          object.url,
-          object.description]),
-        err => console.error(err);
-      });
-    });
-}
 
 app.get('/projects/seattle', (req, res) => {
-  client.query(`SELECT location, dates, projectname, url, description FROM seattle;`)
-    .then(results => res.send(results.rows))
-    .catch(console.err);
+  client.query(`SELECT COUNT (*) FROM seattle;`)
+    .then(data => {
+      if (data.rowCount < 2) {
+        let url = 'https://data.seattle.gov/resource/2ey2-escs.json';
+        superagent.get(url)
+          .then(data => {
+            data.body.forEach(object => {
+              client.query(`INSERT INTO seattle (location, dates, projectname, url, description) 
+              VALUES ($1, $2, $3, $4, $5)`,
+              [object.location,
+                object.dates,
+                object.projectname,
+                object.url,
+                object.description]),
+              err => console.error(err);
+            });
+          });
+      }
+      client.query(`SELECT location, dates, projectname, url, description FROM seattle;`)
+        .then(results => res.send(results.rows))
+        .catch(console.err);
+    });
 });
 
 app.get('/', (req,res) => res.redirect(CLIENT_URL));
